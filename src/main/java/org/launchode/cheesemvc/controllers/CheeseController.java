0adde8758d0692daff61265bd9e1125404780d71
JasonOnes <jasonr.jones14@gmail.com>
2017-08-09 15:53:11 -0700

    Initial commit

---
+++ src/main/java/org/launchode/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,43 @@
+package org.launchode.cheesemvc.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.ArrayList;
+
+
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+
+    ArrayList<String> cheeses = new ArrayList<>();
+
+    @RequestMapping(value="")
+
+    public String index(Model model){
+
+
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "Sweet Cheesus!");
+        return "cheese/index"; //base prefix + template name
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public String  processAddCheeseForm(@RequestParam String cheeseName) {
+
+        cheeses.add(cheeseName);
+
+        return "redirect:"; //by leaving :blank redirects to base handler
+
+    }
+
+}
